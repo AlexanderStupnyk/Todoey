45c3b4472accbe994238add48af0de77ff868c8c
Stupnyk Alex <e-mailbox85@ukr.net>
2018-01-22 12:30:00 +0200

    Convert to MVC design pattern

---
+++ Todoey/DataModel/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by alex on 22.01.18.
+//  Copyright © 2018 alex. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title : String = ""
+    var done : Bool = false
+}
